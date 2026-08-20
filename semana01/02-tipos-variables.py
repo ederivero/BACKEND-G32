2d65758632a024c00430ab9d9aa3c0f3aa0473a6
Eduardo <ederiveroman@gmail.com>
2026-08-19 19:26:54 -0500

    feat: inicio sesion 2 semana 1

---
+++ semana01/02-tipos-variables.py
@@ -0,0 +1,16 @@
+# String (texto)
+# Se identifican por tener comilla simple o doble
+# Se puede crear strings pero de varias lineas usando la tiple (doble) comilla 
+nombre = 'Eduardo O\'Conner'
+print(nombre)
+nombre = "Eduardo O'Conner"
+
+# al poner el caracter 'r' al comienzo del string este interpretara el uso del back-slash como un caracter mas y no para poder hacer uso de caracteres especiales
+ruta = r'C:\documents\etc'
+print(ruta)
+texto = '''Hola soy su profesor.
+El dia de hoy continuaremos avanzando con Python.
+    Hoy haremos varios ejercicios'''
+
+apellido = "De Rivero"
+
