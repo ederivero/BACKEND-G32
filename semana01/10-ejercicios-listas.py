2367ab97aba061f49d70cfba729159b502a0307f
Eduardo <ederiveroman@gmail.com>
2026-08-19 22:41:46 -0500

    chore: uso de listas

---
+++ semana01/10-ejercicios-listas.py
@@ -0,0 +1,6 @@
+# 1. Crear una lista de 5 productos, luego, agregar uno nuevo, eliminar el 2do producto y ordenar alfabeticamente los productos e imprimirlos
+
+
+# 2. tengo la siguiente lista (matriz)
+matriz = [[1,2],[3,4]]
+# Como hago para obtener el '3'
