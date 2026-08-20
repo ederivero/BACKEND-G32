51c54823fd288a905609a82db5099e06980f6730
Eduardo <ederiveroman@gmail.com>
2026-08-19 20:10:23 -0500

    feat: operadores aritmeticos

--- semana01/02-tipos-variables.py
+++ semana01/02-tipos-variables.py
@@ -14,3 +14,28 @@ El dia de hoy continuaremos avanzando con Python.
 
 apellido = "De Rivero"
 
+persona = 'Eduardo'
+# el prefijo f lo que hace es que lo que ponga entre {} podra ser codigo python
+saludo = f'Hola {persona}, mucho gusto'
+print(saludo)
+# Si no quieres usar el prefijo f
+# para el uso del metodo format la misma cantidad de llaves tiene que haber con la misma cantidad de parametros
+saludo = 'Hola {}, mucho gusto'.format(persona)
+print(saludo)
+
+# Enteros o Int
+edad = 30
+
+# Decimales o Float
+estatura = 1.88
+
+# Boolean 
+aprobado = True 
+viudo = False
+
+# Las variables en python nunca pueden empezar con numeros, tampoco con caracteres especiales\
+# no se recomienda empezar con _ porque puede malentederse con encapsulamiento de POO
+
+# Para saber el tipo de variable se usa la funcion type
+print(type(saludo))
+
