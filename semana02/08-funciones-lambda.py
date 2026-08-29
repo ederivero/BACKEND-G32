911b3ee0be7d86de255ebce8c74babb0e11e76eb
Eduardo <ederiveroman@gmail.com>
2026-08-28 20:52:02 -0500

    feat: lambda functions

---
+++ semana02/08-funciones-lambda.py
@@ -0,0 +1,6 @@
+# en python tenemos la posibilidad de crear funciones de una sola linea y esta se conocen como funciones lambda
+# Automaticamente retorna el resultado de la operacion
+sumar = lambda num1, num2: num1+num2
+
+resultado = sumar(10,20)
+print(resultado)
