5634fa4ab87a5c7f4ffaa02f770d29693b86c764
Eduardo <ederiveroman@gmail.com>
2026-08-28 23:00:13 -0500

    chore: ejercicios

---
+++ semana02/11-ejercicios.py
@@ -0,0 +1,10 @@
+# 1. Crea una funcion calcular_igv en la cual pide el precio y retorne el precio final aplicado el igv (18%) - puede utilizar lambda
+# 2. Convierte la temperatura en la funcion cambiar_temperatura de Celcius a Farenheit - puede utilizar lambda
+# 3. Dado un diccionario de un producto (nombre, precio, stock) utiliza if-elif-else para clasificar el stock en "Sin Stock" (si el stock es 0), "Stock Bajo" (si el stock es entre 1 y 10) y "Disponible"(si el stock es mas que 10)
+# 4. Usando un while, simula un cajero automatico simple que pida una clave hasta que el usuario la ingrese correctamente, usando 3 intentos como maximo, sino indica que la cuenta fue bloqueada.
+# 5. Crear una funcion calcular_area_circulo(radio) que retorne el area (3.1415 como valor de pi) - puede utilizar lambda
+# 6. Crear una funcion procesar_notas(nombre, *notas) que calcule y retorne el promedio y luego clasifique el resultado con if-elif-else en una segunda funcion clasificar(promedio)
+# 7. En una lista de 5 elementos crear una funcion obtener_por_indice(lista, indice) que capture el error IndexError si el indice no existe
+
+# BONUS!
+# 8. Crear una funcion con while True que pida numeros al usuario y los sume manejando un try-except en el que caso que se ingrese un texto en vez de numeros y que al escribir Salir, termine la sumatoria sin lanzar el error
