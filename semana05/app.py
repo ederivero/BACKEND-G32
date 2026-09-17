10f7b937e72c301281d163333415698df6373844
Eduardo <ederiveroman@gmail.com>
2026-09-16 21:21:14 -0500

    feat: conectando python con bd

--- semana05/app.py
+++ semana05/app.py
@@ -1,5 +1,22 @@
 # from LIBRERIA import CLASES, FUNCIONES que queremos usar de la libreria
 from flask import Flask, request
+from werkzeug.exceptions import UnsupportedMediaType
+# PARA MSSQL
+from mssql_python import connect as conector_mssql
+connection_str = "Server=localhost,1433;Database=<database_name>;UID=<username>;PWD=<password>;Encrypt=yes;TrustServerCertificate=yes"
+
+# conexion_mssql = conector_mssql(connection_str)
+
+# cursor_mssql = conexion_mssql.cursor()
+
+# PARA POSTGRES
+from psycopg import connect
+
+# postgresql://NOMBRE_USUARIO:PASSWORD_USUARIO@HOST:PUERTO/NOMBRE_BD
+credenciales = "postgresql://postgres:root@127.0.0.1:5432/flask_db"
+conexion = connect(conninfo=credenciales)
+
+
 # request nos dara toda la informacion proveniente del cliente, y solamente puede ser llamado dentro de un controlador > https://flask.palletsprojects.com/en/stable/api/#flask.Request
 
 # __name__ > Variable global de python que sirve para indicar si el archivo en el cual nos encontramos se esta ejecutando directamente o no en la terminal 
@@ -34,18 +51,51 @@ def estado_servidor():
 def gestionar_productos():
     print(request.method)
     if request.method == 'GET':
+        # El encargado de iniciar la comunicacion con la BD
+        cursor = conexion.cursor()
 
+        # Ejecutamos el comando en la bd, aca no es necesario colocar ; al final
+        cursor.execute("SELECT * FROM productos")
+
+        # Para obtener el resultado (si es necesario) usamos los metodos fetchone, fetchall, fetchmany
+        productos_bd = cursor.fetchall()
+
+        print(productos_bd)
+        resultado = []
+
+        for producto in productos_bd:
+            resultado.append({
+                "id": producto[0],
+                "nombre": producto[1],
+                "precio": float(producto[2]),
+                "cantidad": producto[3]
+            })
+            
         # Los controladores (es la logica del endpoint) suelen retornar diccionarios que estos seran interpretados en JSON o tambien se suele retornar listas (arreglos)
         return {
             "message":"Los productos son:",
-            "content": productos
+            "content": resultado
         }
     
     elif request.method =='POST':
-
-        return {
-            "message":"Producto creado exitosamente"
-        }
+        # Se usa para crear nueva informacion proveniente del frontend
+        # request.get_data() # retorna la informacion proveniente del cliente directamente como si fuese un string, sirve tambien si la informacion enviada sera un texto
+        # print(request.get_data())
+        # request.get_json() # retorna la informacion del cliente y la convierte a un diccionario para que python la pueda entender
+        # print(request.get_json())
+        try:
+            data = request.get_json()
+            # Ahora con la informacion correctamos agregamos este producto a nuestra lista
+            productos.append(data)
+            return {
+                "message":"Producto creado exitosamente"
+            }
+        except UnsupportedMediaType:
+            # Handler (manejador de errores)
+            return {
+                "message": "Debes enviar la informacion en formato JSON"
+            }
+        
 
 
 
